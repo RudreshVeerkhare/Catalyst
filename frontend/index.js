6e355d5e9627f40f71e04a8cfa44a4c3c753b800
RudreshVeerkhare <veerkharerudresh@gmail.com>
2021-03-14 18:40:02 +0530

    :sparkles Added Pinch Zoom feature

--- frontend/index.js
+++ frontend/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.css';
-import App from './src/App.jsx';
-import { ThemeProvider } from './src/contexts/ThemeContext';
+import React from "react";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./src/App.jsx";
+import { ThemeProvider } from "./src/contexts/ThemeContext";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 render(
-    <ThemeProvider><App /></ThemeProvider>,
-    document.getElementById('root')
-)
+    <TransformWrapper
+        wheel={{ wheelEnabled: false, touchPadEnabled: true, step: 35 }}
+        doubleClick={{ mode: "reset" }}
+        scalePadding={{ disabled: true }}
+        pan={{ padding: true, paddingSize: 0 }}
+        zoomIn={{ animation: false }}
+        zoomOut={{ animation: false }}
+    >
+        <TransformComponent>
+            <ThemeProvider>
+                <App />
+            </ThemeProvider>
+        </TransformComponent>
+    </TransformWrapper>,
+
+    document.getElementById("root")
+);
